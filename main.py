db46943617ce2bad7f3a042e283e1ff0215225bd
Mohammad Yousry <mohammadyousry187@gmail.com>
2025-06-02 21:03:10 +0300

    Sync test

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.responses import HTMLResponse
 import secrets
 
+print("Testing sync")
+
+
 # === Routers ===
 from routers import (
     users, pending_approvals, doctor_assignments,
