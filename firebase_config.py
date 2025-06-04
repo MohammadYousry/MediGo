2b588833f72faac022f0734daa0f6d5dce83d320
Mohammad Yousry <mohammadyousry187@gmail.com>
2025-06-04 23:41:46 +0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,14 +1,15 @@
+# firebase_config.py
+
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
 
-# ✅ التحقق من أن Firebase لم يتم تهيئته مسبقًا
+# ✅ تهيئة Firebase مرة واحدة فقط
 if not firebase_admin._apps:
-    cred = credentials.Certificate("/etc/secrets/firebase_key.json")  # أو المسار الصحيح
+    cred = credentials.Certificate("/etc/secrets/firebase_key.json")  # غيّر للمسار الصحيح
     firebase_admin.initialize_app(cred, {
         'storageBucket': 'medi-go-eb65e.firebasestorage.app'
     })
 
-
-# ✅ بعد التهيئة نقدر نستخدمهم بأمان
+# ✅ العملاء الجاهزين
 db = firestore.client()
-bucket = storage.bucket()  # الآن يعمل بدون مشاكل
+bucket = storage.bucket()
