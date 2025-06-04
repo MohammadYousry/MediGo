2e60e3b2bf817784af28c2b6edd3715ddb230a59
Mohammad Yousry <mohammadyousry187@gmail.com>
2025-06-04 21:38:06 +0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,21 +1,13 @@
 import firebase_admin
-from firebase_admin import credentials, storage, firestore # أضفت firestore هنا احتياطًا لو بتحتاجوه
+from firebase_admin import credentials, storage, firestore
 
-# Initialize Firebase Admin SDK with credentials
-# عدّل السطر التالي ليشير إلى اسم ملف المفتاح الخاص بك مباشرة
 cred = credentials.Certificate("/etc/secrets/firebase_key.json")
 
-# Initialize the app only if it hasn't been initialized yet
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred, {
-        'storageBucket': 'medi-go-eb65e.firebasestorage.app'
+        'storageBucket': 'medi-go-eb65e.appspot.com'  # ✅ ده الصح
     })
 
-
-# Firestore client
 db = firestore.client()
-
-# You can get a reference to the bucket (optional, if you use storage)
-# bucket = storage.bucket() # اسم الـ bucket هو نفسه اللي فوق بدون gs://
 
 print("Firebase App initialized successfully with Firestore and Storage!")
