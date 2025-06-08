8b4d1852d062413d83fbc4d97c87a914856b1ab8
Mohammad Yousry <mohammadyousry187@gmail.com>
2025-06-08 20:24:05 +0300

    Update admin.py

--- routers/admin.py
+++ routers/admin.py
@@ -12,16 +12,6 @@ router = APIRouter(prefix="", tags=["Admin"])
 # Optional: Enable CORS if needed for frontend access
 origins = ["*"]
 
-from fastapi import FastAPI
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-app.include_router(router)
 
 @router.get("/facilities")
 def search_facilities(name: str = Query("", alias="name"), id: str = Query("", alias="id")):
